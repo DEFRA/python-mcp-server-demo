68be48f8fad3b260401aa33391b9d6e5cf3d57bb
sfitz42 <87677173+sfitz42@users.noreply.github.com>
2025-08-07 16:58:21 +0100

    Add fake weather tool

--- app/mcp/demo_server/server.py
+++ app/mcp/demo_server/server.py
@@ -1,9 +1,28 @@
+from pydantic import BaseModel
 from mcp.server.fastmcp import FastMCP
+from random import randint
+
+
+class WeatherData(BaseModel):
+    location: str
+    temperature: str
+    condition: str
 
 demo_mcp_server = FastMCP("Python MCP Server Demo")
+
 
 @demo_mcp_server.tool()
 def add(x: int, y: int) -> int:
     """Add two numbers."""
     return x + y
 
+
+@demo_mcp_server.tool()
+def get_weather(location: str) -> WeatherData:
+    """Get the current weather in a given location"""
+
+    return WeatherData(
+        location=location,
+        temperature=f"{randint(1, 30)}°C",
+        condition="Sunny"
+    )
